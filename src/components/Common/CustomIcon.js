6aa83756d8d8fb63e0325893977589d8871655b9
jw.kim <babamba88@gintlab.com>
2020-07-14 16:16:57 +0900

    custom icon file

--- src/components/Common/CustomIcon.js
+++ src/components/Common/CustomIcon.js
@@ -2,7 +2,7 @@ import { createFromIconfontCN } from '@ant-design/icons';
 
 //https://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=1871992
 const MyIcon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1871992_l3td9qv636.js' // 在 iconfont.cn 上生成
+  scriptUrl: require('../../assets/icon/cdn') // 在 iconfont.cn 上生成
 });
 
 export default MyIcon;

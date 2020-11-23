3a294f3cc63be7c29e201c07278e7e200d1ef5cc
jw.kim <babamba88@gintlab.com>
2020-11-23 17:11:33 +0900

    fix: :lipstick: fix

--- src/pages/DeckFolio.tsx
+++ src/pages/DeckFolio.tsx
@@ -119,7 +119,7 @@ const DeckFolio = () => {
       exit="out"
       variants={pageVariants}
       transition={pageTransition}
-      style={{ position: "absolute", width: "100%", height: "100%", zIndex: 2 }}
+      style={{ position: "absolute", width: "100%", height: "100%" }}
     >
       <HeadMeta
         title="JW Project PortFolio"
